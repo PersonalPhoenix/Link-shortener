cd92c032fcbe225f0faba25df8730e8c524f26e1
PersonalPhoenix <applmacter@mail.ru>
2023-08-05 22:00:06 +0300

    modification admin

--- link_shorter/makelink/admin.py
+++ link_shorter/makelink/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import Link
 
 
-admin.site.register(Link)
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['url', 'uuid']
